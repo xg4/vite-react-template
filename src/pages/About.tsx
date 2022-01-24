49ed569dad32155ab2c9c8229c00c70104a098dc
xg4 <xingor4@gmail.com>
2022-01-24 15:53:14 +0800

    chore(deps): add deps

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,12 @@
+import { map } from 'lodash'
 import React from 'react'
 
 function About() {
+  console.log(
+    map([1, 2, 3, 4], (i) => i * 2),
+    'lodash map'
+  )
+
   return <div>about</div>
 }
 
