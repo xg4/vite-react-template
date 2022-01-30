1b5a1cfa6ba8674d9b9331a7a82b257c48a17f60
xg4 <xingor4@gmail.com>
2022-01-30 12:15:16 +0800

    chore: add deps

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,13 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    react(),
     rollupReplace({
       __ENV__: process.env.NODE_ENV,
       __DATE__: dayjs().format('YYYY-MM-DD HH:mm:ss'),
     }),
-    react(),
   ],
   resolve: {
-    alias: { lodash: 'lodash-es' },
+    alias: { 'lodash/fp': 'lodash/fp', lodash: 'lodash-es' },
   },
 })
