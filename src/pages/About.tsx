e63c19dc5d1168c40400a846672a8c26efa7edb9
xg4 <xingor4@gmail.com>
2022-01-30 16:57:43 +0800

    chore: add git hook, husky

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,13 +1,13 @@
-import { map } from 'lodash'
-import React from 'react'
+import { map } from 'lodash';
+import React from 'react';
 
 function About() {
   console.log(
     map([1, 2, 3, 4], (i) => i * 2),
-    'lodash map'
-  )
+    'lodash map',
+  );
 
-  return <div>about</div>
+  return <div>about</div>;
 }
 
-export default About
+export default About;
