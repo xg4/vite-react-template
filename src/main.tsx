3be51bb158f2cb0e5384b6931580e8f558492fb8
xg4 <xingor4@gmail.com>
2022-06-06 11:48:41 +0800

    refactor: rename tailwind.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './styles/index.css'
+import './styles/tailwind.css'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
