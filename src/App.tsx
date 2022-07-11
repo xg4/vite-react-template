4058bfa80f6bc63a08df58c5b3a5be114aad3547
xg4 <xingor4@gmail.com>
2022-07-11 14:13:41 +0800

    chore: update deps
    chore: update react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { QueryClient, QueryClientProvider } from 'react-query'
-import Routes from './routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter as Router } from 'react-router-dom'
+import Routes from './routes'
 
 const queryClient = new QueryClient()
 
